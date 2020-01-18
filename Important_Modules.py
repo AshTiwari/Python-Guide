1273c80844232a163812188355230e6af4edc344
AshTiwari <ashu.atiwa@gmail.com>
2020-01-18 21:02:12 +0530

    pyperclip(), sys.exit()

---
+++ Important_Modules.py
@@ -0,0 +1,15 @@
+# important modules
+
+###### 1######
+import sys
+#sys.exit()     #Used to end program prematurely.
+
+
+###### 2 ######
+import pyperclip                #used to copy and paste stuff at clipboard
+pyperclip.copy('HELLO')
+pyperclip.paste()
+
+###### 3 ######
+
+
