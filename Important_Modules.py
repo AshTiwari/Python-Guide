580450b8527f8a3df1d30f149b6501c1e91ad065
AshTiwari <ashu.atiwa@gmail.com>
2020-01-23 01:26:21 +0530

    4 modules

--- Important_Modules.py
+++ Important_Modules.py
@@ -1,15 +1,24 @@
 # important modules
 
-###### 1######
+#1
 import sys
 #sys.exit()     #Used to end program prematurely.
 
 
-###### 2 ######
+#2
 import pyperclip                #used to copy and paste stuff at clipboard
 pyperclip.copy('HELLO')
-pyperclip.paste()
 
-###### 3 ######
+#if we copy something outside the python program and pyper paste it on a variable,
+#the variable will store the data.
+vaariable = pyperclip.paste()               
 
+#3
+import re
 
+regex = re.compile(r'.')
+mo = regex.search('message')
+mo.group()
+regex.findall('message')
+
+#4
