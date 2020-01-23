1acc4a9b74c17843ea4149c04310d14aa2667721
AshTiwari <ashu.atiwa@gmail.com>
2020-01-23 11:42:42 +0530

    OS modules and functions.

--- os_module.py
+++ os_module.py
@@ -6,9 +6,11 @@ import os
 print('\nos.path.join() prints os suitable path.')
 print(os.path.join('root folder','folder','file.txt'))   
 
+#get current working directory.
 print('\nos.getcwd() prints current working directory.')
 print(os.getcwd())
 
+#change directory.
 print('\nos.chdir() changes current working directory.')
 os.chdir('C://')
 print(os.getcwd())
@@ -22,6 +24,7 @@ print('\nNotation \t Meaning')
 print('. \t\t current directory')
 print('.. \t\t parent directory')
 
+#absolute path and check if it is absolute path.
 print('os.path.abspath() gives the absolute path of a file.')
 print(os.path.abspath('users'))
 
@@ -30,6 +33,7 @@ path = os.path.isabs('C://users')
 print(path)
 
 
+#relative path between two paths.
 print('\nos.path.relpath() gives relative path between first and second argument.')
 path = os.path.relpath('C://users//folder1//folder2','C://users')
 print(path)
@@ -37,6 +41,7 @@ path = os.path.relpath('C://users','C://users//folder1//folder2')
 print(path)
 
 
+#Seprate directory and file name.
 print('\nSeprating directory name and base name.')
 dirname = os.path.dirname('C://users//folder1//folder2//spam.png')
 print(dirname)
@@ -44,20 +49,33 @@ basename = os.path.basename('C://users//folder1//folder2//spam.png')
 print(basename)
 
 
+#check if path exists.
 print('\nCheck if a path exists.')
 print(os.path.exists('C://users//folder1//folder2//spam.png'))
 print(os.path.exists('C://users'))
 print(os.path.exists('C:\Windows\System32//calc.exe'))
 
 
+#check if it is a file or a directory.
 print('\nCheck is path belong to file or folder and actually exist.')
 print(os.path.isfile('C:\Windows\System32//calc.exe'))
 print(os.path.isdir('C:\Windows\System32'))
 
 
+#get size of a file and list the directories.
 print('\nGet size of file and list of files in a folder.')
 print(os.path.getsize('C:\Windows\System32//calc.exe'))
 print(os.listdir('C://'))
+
+
+#make a new directory.
+print('\nMake a new directory in documents.')
+
+try:
+    os.makedirs('C://Users//Public//Documents//filename.txt')
+except FileExistsError:
+    print('File already exist.')
+print(os.path.exists('C://Users//Public//Documents//filename.txt'))
 
 
 
