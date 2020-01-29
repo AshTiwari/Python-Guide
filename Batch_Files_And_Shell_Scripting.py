15a290b42fbb5251142e9b9eddb52231dc9ff60e
AshTiwari <ashu.atiwa@gmail.com>
2020-01-29 19:31:38 +0530

    How to run python program from command prompt or run administration using batch.bat file. Also, find the attached Batch.bat file.

---
+++ Batch_Files_And_Shell_Scripting.py
@@ -0,0 +1,22 @@
+#! python3
+
+#run from command line.
+#Batch files or Shell Scripts.
+'''
+-Open run administration: Window Key + r.
+-Type 'C://GitHub Projects//Python-Guide//Batch.bat'
+-It will open batch file which has the code to
+run this python file and also hold the screen until we press any key.
+
+-We can run the the batch file directly
+i.e. only type Batch.bat in Run administration but for that
+we have to save the path to the folder.
+i.e save the path to "Python-Guide".
+'''
+
+
+print('Box.')
+print('*'*20)
+for i in range(1,10):
+    print('*'+' '*18+'*')
+print('*'*20)
