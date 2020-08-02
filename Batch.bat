e31c4de6d274d01c34fea695a79ca098441dede9
AshTiwari <ashu.atiwa@gmail.com>
2020-08-02 13:46:59 +0530

    Remove Files

--- Batch.bat
+++
@@ -1,6 +0,0 @@
-@py "C:\GitHub Projects\Python-Guide\Batch_Files_And_Shell_Scripting.py" %*
-@pause
-
-
-rem starting with @ means apply following command but donot print it.
-rem %* means forward all the command line arguments to the python program.
