3e2d435d62dc9fcd45ea08a1c5a2721c3f0292b2
AshTiwari <ashu.atiwa@gmail.com>
2020-08-02 13:44:49 +0530

    Remove Files.

--- Escape_Sequence.py
+++
@@ -1,8 +0,0 @@
-#More on Strings.
-
-#Escape Sequences from github
-
-#raw strings.
-print(r'Printing all raw string except quote  \ \ \n')
-print(''' Printing all raw string ' ' \ \ \n ''')
-print(""" Printing all raw string ' ' \ \ \n """)
--- OOPS_Class_Composition_vs_Aggregation.py
+++
@@ -1,7 +0,0 @@
-# difference between class composition and aggregation.
-
-print('1.')
-print('-In composition, if the object of second class deletes then')
-print(' the object of first classs automatically deletes.')
-print('-But, in aggregation both the objects are intanstiated seperately,')
-print(' so they dont have an effect on each other.')
