7fa1b50f8de7336871d8f8a20f6c7b1652f80f49
AshTiwari <ashu.atiwa@gmail.com>
2021-02-07 18:42:54 +0530

    Implemented different versions of Binary Search.

--- Algorithms/binarySearch.py
+++ Algorithms/binarySearch.py
@@ -1,13 +1,10 @@
 # Find element in an sorted array.
 
-def binarySearch(array,element,freq = 0):
+def binarySearch(array, element, mode = 0):
 	n = len(array)
-	left = 0
-	right = n-1
+	left, right = 0, n-1
 	mid = (left+right)//2
-	found = 0
-	frequency = 0
-	
+	found = 0	
 	while(left <= right):		
 		if array[mid] == element:			
 			found = 1
@@ -18,27 +15,58 @@ def binarySearch(array,element,freq = 0):
 		elif array[mid] < element:
 			left = mid + 1
 			mid = (left+right)//2
-	
-	if freq != 0 and found ==0:		
-		return -1,0
-	elif freq != 0 and found == 1:
-		i = mid + 1
-		while(i < n and array[i] == element ):
-			frequency += 1
-			i += 1
+
+	if mode == 0:
+		return mid if found == 1 else -1
+
+	else:
 		least_index = mid 
 		while(least_index > -1 and array[least_index] == element ):
-			frequency += 1
 			least_index -= 1
-		return least_index+1, frequency
-				
-		
-		
-	return mid if found == 1 else -1
+		least_index += 1
+		if mode == 1: 
+			return least_index if found == 1 else -1 
+		elif mode == 2:
+			return least_index if found == 1 else (left, right, n)
+		elif mode == 3:
+			frequency, index = 0, least_index
+			while(index < n and array[index] == element ):
+				frequency += 1 
+				index += 1
+			return (least_index, frequency) if found == 1 else (-1, frequency)
+
+
+
+def leastIndexBinarySearch(array, element):
+	return binarySearch(array, element, 1)
+
+def BinarySearchWithFrequency(array, element):
+	return binarySearch(array, element, 3)
+
+def lessThanBinarySearch(array, element):
+	input =  binarySearch(array,element, 2)
+	if isinstance(input, int):
+		return input
+	elif isinstance(input, tuple):
+		left, right, n = input
+		return right
+
+def GreaterThanBinarySearch(array, element):
+	input =  binarySearch(array,element, 2)
+	if isinstance(input, int):
+		return input 
+	elif isinstance(input, tuple):
+		left, right, n = input
+		return left if left < n else -1
+
 
 if __name__ == "__main__":
 	array = [0, 1, 2, 3, 5, 5, 5, 5, 7, 8, 9]
 	#array = [1]
-	element = 5
-	print(binarySearch(array,element))
-	print(binarySearch(array,element,1))
+	element = 4s
+	#print(binarySearch(array,element))
+	#print(leastIndexBinarySearch(array,element))
+	#print(BinarySearchWithFrequency(array,element))
+	print(lessThanBinarySearch(array,element))
+	print(GreaterThanBinarySearch(array,element))
+	
