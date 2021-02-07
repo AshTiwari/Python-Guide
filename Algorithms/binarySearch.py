ae19806b4db09cd5962fe01574cff27b0531f94c
AshTiwari <ashu.atiwa@gmail.com>
2021-02-07 20:54:48 +0530

    Implementing different versions of Binary Search.

--- Algorithms/binarySearch.py
+++ Algorithms/binarySearch.py
@@ -37,13 +37,13 @@ def binarySearch(array, element, mode = 0):
 
 
 
-def leastIndexBinarySearch(array, element):
+def firstInstanceBinarySearch(array, element):
 	return binarySearch(array, element, 1)
 
 def BinarySearchWithFrequency(array, element):
 	return binarySearch(array, element, 3)
 
-def lessThanBinarySearch(array, element):
+def lessThanEqualBinarySearch(array, element):
 	input =  binarySearch(array,element, 2)
 	if isinstance(input, int):
 		return input
@@ -51,7 +51,7 @@ def lessThanBinarySearch(array, element):
 		left, right, n = input
 		return right
 
-def GreaterThanBinarySearch(array, element):
+def GreaterThanEqualBinarySearch(array, element):
 	input =  binarySearch(array,element, 2)
 	if isinstance(input, int):
 		return input 
@@ -63,10 +63,10 @@ def GreaterThanBinarySearch(array, element):
 if __name__ == "__main__":
 	array = [0, 1, 2, 3, 5, 5, 5, 5, 7, 8, 9]
 	#array = [1]
-	element = 4s
-	#print(binarySearch(array,element))
-	#print(leastIndexBinarySearch(array,element))
-	#print(BinarySearchWithFrequency(array,element))
-	print(lessThanBinarySearch(array,element))
-	print(GreaterThanBinarySearch(array,element))
+	element = 4
+	print(binarySearch(array,element))
+	print(firstInstanceBinarySearch(array,element))
+	print(BinarySearchWithFrequency(array,element))
+	print(lessThanEqualBinarySearch(array,element))
+	print(GreaterThanEqualBinarySearch(array,element))
 	
