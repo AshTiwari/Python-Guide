39527f1cf668e74f099d6df65fcb959b3c70ccba
AshTiwari <ashu.atiwa@gmail.com>
2021-03-19 19:08:03 +0530

    Imporved Counting Sort.

--- Algorithms/counting_sort.py
+++ Algorithms/counting_sort.py
@@ -1,23 +1,34 @@
 # counting sort.
 
-def counting_sort():   
-    count = [0 for i in range(10)]
+def countingSortPythonic(array, biggestElement):
+    count = [0 for i in range(biggestElement + 1)]
+    for i in array:
+        count[i] += 1
+    sorted_array = []
+    for element,freq in enumerate(count):
+        for j in range(freq):
+            sorted_array.append(element)
+            
+    return sorted_array
+        
+def counting_sort(input_array, biggestElement):   
+    count = [0 for i in range(biggestElement + 1)]
     for i in input_array:
         count[i] +=1
-    #print(count)
 
-    for i in range(1,10):
+    for i in range(biggestElement + 1):
         count[i] += count[i-1]
-    #print(count)
 
     sorted_array = [0 for i in range(len(input_array))]
     for i in input_array:
         index = count[i]
         count[i] -=1
         sorted_array[index-1] = i
-    print(sorted_array)
 
+    return sorted_array
 
-print('Enter the space seperated array.')
-input_array = list(map(int,input().split()))
-#print(input_array)
+if __name__ == "__main__":
+    print('Enter the space seperated array.')
+    input_array = list(map(int,input().split()))
+    print(counting_sort(input_array,9))
+    print(countingSortPythonic(input_array, 9))
